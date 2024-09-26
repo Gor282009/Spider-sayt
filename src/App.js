9ea1c3a3c346f41d18f8a9c5dbdbdc15b6cf637d
Gor <vardanyangor46@gmail.com>
2024-09-26 17:53:43 +0400

    Slider Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
 import MovieItem from './Components/MovieItem/MovieItem';
+import Swiper from './Components/Swiper';
 
 function App() {
   return (
--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -1,18 +1,43 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { useEffect, useState } from "react";
+
+const api_key = "api_key=9b702a6b89b0278738dab62417267c49";
+const main_url = "https://api.themoviedb.org/3";
+const api_url = main_url + "/discover/movie?sort_by=popularity.desc&" + api_key;
+const img_url = "https://image.tmdb.org/t/p/w500";
+const searchUrl = main_url + "/search/movie?" + api_key;
 
 export default () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(api_url)
+      .then((response) => response.json())
+      .then((data) => setData(data.results))
+      .catch((error) => console.error("Error:", error));
+    console.log(data);
+  }, []);
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <div className="w-[1000px]">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        className="w-full"
+      >
+        {data.map((movie) => (
+          <div key={movie.id} className="">
+            <SwiperSlide>
+              <img
+                src={img_url + movie.poster_path}
+                alt=""
+                className="w-[251px] h-[171px] object-cover"
+              />
+            </SwiperSlide>
+          </div>
+        ))}
+      </Swiper>
+    </div>
   );
 };
